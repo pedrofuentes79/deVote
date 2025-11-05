7fd972639eff6f8635e15bad8597064a27eb7c9a
Pedro Fuentes <pedrofuentes7799@gmail.com>
2025-11-05 20:54:44 -0300

    una implementacion mejor, que solamente deja desencriptar al dueño! antes dejaba desencriptar el _count a cualquier votante. Ahora el votante, mientras se esta votando, solamente puede ver su voto. Luego, una vez que el owner desencripta _totalVotes todos lo pueden ver

--- contracts/FHEVoter.sol
+++ contracts/FHEVoter.sol
@@ -1,43 +1,94 @@
 // SPDX-License-Identifier: BSD-3-Clause-Clear
 pragma solidity ^0.8.24;
 
-import { FHE, euint32, externalEuint32, ebool, externalEbool } from "@fhevm/solidity/lib/FHE.sol";
-import { SepoliaConfig } from "@fhevm/solidity/config/ZamaConfig.sol";
-
-// IDEA: usar ZKPs para verificar que lo que me da el usuario es un numero entre 0 y 1 (cuando esto sea un voting system
-
+import {FHE, euint32, externalEuint32, ebool, externalEbool} from "@fhevm/solidity/lib/FHE.sol";
+import {SepoliaConfig} from "@fhevm/solidity/config/ZamaConfig.sol";
 
 // inherits from SepoliaConfig to enable fhEVM support
 contract FHEVoter is SepoliaConfig {
-
     euint32 private _count;
-    mapping(address => bool) private _voters;
     euint32 private _constantOne;
     euint32 private _constantZero;
+    mapping(address => ebool) private _individualVotes;
+    uint32 private _totalVotes; // =0 by default. It is only used when the owner calls "decryptCount"
+    address private _owner;
+
 
     constructor() {
+        _owner = msg.sender;
         _constantOne = FHE.asEuint32(1);
         _constantZero = FHE.asEuint32(0);
         FHE.allowThis(_constantOne);
         FHE.allowThis(_constantZero);
     }
 
-
     // using a boolean allows us to ensure what we add is always 0 or 1
     function vote(externalEbool externalYesOrNo, bytes calldata proof) external {
-        require(!_voters[msg.sender], "Voter already voted");
         ebool yesOrNo = FHE.fromExternal(externalYesOrNo, proof);
         euint32 voteToAdd = FHE.select(yesOrNo, _constantOne, _constantZero);
+
+        _individualVotes[msg.sender] = yesOrNo;
+        FHE.allow(_individualVotes[msg.sender], msg.sender); // allows the sender to decrypt ITS VOTE
+        FHE.allowThis(_individualVotes[msg.sender]); // allows the contract to use this value too
 
         _count = FHE.add(_count, voteToAdd);
 
         FHE.allowThis(_count);
-        FHE.allow(_count, msg.sender);
-        _voters[msg.sender] = true;
     }
 
     function getCount() external view returns (euint32) {
         return _count;
     }
+
+    function getMyVote() external view returns (ebool) {
+        return _individualVotes[msg.sender];
+    }
+
+    function requestDecryption() external {
+        require(msg.sender == _owner, "Only owner can decrypt the count");
+
+        bytes32[] memory cypherTexts = new bytes32[](1);
+        cypherTexts[0] = FHE.toBytes32(_count);
+            FHE.requestDecryption(
+            // the list of encrypted values we want to publc decrypt
+            cypherTexts,
+            // the function selector the FHEVM backend will callback with the clear values as arguments
+            this.callbackDecryptSingleUint32.selector
+        );
+    }
+
+    function getDecryptedCount() external view returns (uint32) {
+        return _totalVotes;
+    }
+
+    function callbackDecryptSingleUint32(uint256 requestID, bytes memory cleartexts, bytes memory decryptionProof) external {
+        // The `cleartexts` argument is an ABI encoding of the decrypted values associated to the
+        // handles (using `abi.encode`). 
+        // 
+        // ===============================
+        //    ☠️🔒 SECURITY WARNING! 🔒☠️
+        // ===============================
+        //
+        // Must call `FHE.checkSignatures(...)` here!
+        //            ------------------------
+        //
+        // This callback must only be called by the authorized FHEVM backend.
+        // To enforce this, the contract author MUST verify the authenticity of the caller
+        // by using the `FHE.checkSignatures` helper. This ensures that the provided signatures
+        // match the expected FHEVM backend and prevents unauthorized or malicious calls.
+        //
+        // Failing to perform this verification allows anyone to invoke this function with
+        // forged values, potentially compromising contract integrity.
+        //
+        // The responsibility for signature validation lies entirely with the contract author.
+        // 
+        // The signatures are included in the `decryptionProof` parameter.
+        //
+        FHE.checkSignatures(requestID, cleartexts, decryptionProof);
+
+        (uint32 decryptedInput) = abi.decode(cleartexts, (uint32));
+        _totalVotes = decryptedInput;
+    }
+
 
 }
--- test/FHEVoter.ts
+++ test/FHEVoter.ts
@@ -5,6 +5,11 @@ import { expect } from "chai";
 import { encryptKeystoreJson } from "ethers";
 import { ethers, fhevm } from "hardhat";
 
+import { HardhatFhevmRuntimeEnvironment } from "@fhevm/hardhat-plugin";
+import * as hre from "hardhat";
+
+
+
 type Signers = {
   deployer: HardhatEthersSigner;
   alice: HardhatEthersSigner;
@@ -26,7 +31,11 @@ describe("FHEVoter", function () {
 
     before(async function () {
         const ethSigners: HardhatEthersSigner[] = await ethers.getSigners();
-        signers = { deployer: ethSigners[0], alice: ethSigners[1], bob: ethSigners[2] };
+        signers = { 
+            deployer: ethSigners[0], 
+            alice: ethSigners[1], 
+            bob: ethSigners[2] 
+        };
     });
 
     beforeEach(async () => {
@@ -49,57 +58,69 @@ describe("FHEVoter", function () {
         const tx = await fheVoterContract.connect(signers.alice).vote(encryptedVoteValue.handles[0], encryptedVoteValue.inputProof);
         await tx.wait();
 
-        const encryptedCount = await fheVoterContract.getCount();
-        const clearCount = await fhevm.userDecryptEuint(
-            FhevmType.euint32,
-            encryptedCount,
+        // verify that my vote was "true"
+        const myVoteEncrypted = await fheVoterContract.connect(signers.alice).getMyVote();
+        const clearMyVote = await fhevm.userDecryptEbool(
+            myVoteEncrypted,
             fheVoterContractAddress,
             signers.alice
         );
-        expect(clearCount).to.eq(1);
+        expect(clearMyVote).to.eq(clearVoteValue);
     });
-
     it("allows to vote with false", async function () {
         const clearVoteValue = false;
         const encryptedVoteValue = await fhevm
             .createEncryptedInput(fheVoterContractAddress, signers.alice.address)
             .addBool(clearVoteValue)
             .encrypt();
-
+            
         const tx = await fheVoterContract.connect(signers.alice).vote(encryptedVoteValue.handles[0], encryptedVoteValue.inputProof);
         await tx.wait();
 
-        const encryptedCount = await fheVoterContract.getCount();
-        const clearCount = await fhevm.userDecryptEuint(
-            FhevmType.euint32,
-            encryptedCount,
+        const myVoteEncrypted = await fheVoterContract.connect(signers.alice).getMyVote();
+        const clearMyVote = await fhevm.userDecryptEbool(
+            myVoteEncrypted,
             fheVoterContractAddress,
             signers.alice
         );
-        expect(clearCount).to.eq(0);
+        expect(clearMyVote).to.eq(clearVoteValue);
     });
 
-    it("does not allow to vote twice", async function () {
-        const clearVoteValue = true;
-        const encryptedVoteValue = await fhevm
+    it("allows to see the final count", async function () {
+        const aliceClearVoteValue = true;
+        const bobClearVoteValue = false;
+
+        const aliceEncryptedVoteValue = await fhevm
             .createEncryptedInput(fheVoterContractAddress, signers.alice.address)
-            .addBool(clearVoteValue)
+            .addBool(aliceClearVoteValue)
             .encrypt();
             
-            
-        const tx = await fheVoterContract.connect(signers.alice).vote(encryptedVoteValue.handles[0], encryptedVoteValue.inputProof);
+        let tx = await fheVoterContract.connect(signers.alice).vote(aliceEncryptedVoteValue.handles[0], aliceEncryptedVoteValue.inputProof);
         await tx.wait();
-        await expect(
-            fheVoterContract.connect(signers.alice).vote(encryptedVoteValue.handles[0], encryptedVoteValue.inputProof)
-        ).to.be.revertedWith("Voter already voted");
 
-        const encryptedCount = await fheVoterContract.getCount();
-        const clearCount = await fhevm.userDecryptEuint(
-            FhevmType.euint32,
-            encryptedCount,
-            fheVoterContractAddress,
-            signers.alice
-        );
+        const bobEncryptedVoteValue = await fhevm
+            .createEncryptedInput(fheVoterContractAddress, signers.bob.address)
+            .addBool(bobClearVoteValue)
+            .encrypt();
+            
+        tx = await fheVoterContract.connect(signers.bob).vote(bobEncryptedVoteValue.handles[0], bobEncryptedVoteValue.inputProof);
+        await tx.wait();
+
+        tx = await fheVoterContract.requestDecryption()
+        await tx.wait();
+
+        // We use the FHEVM Hardhat plugin to simulate the asynchronous onchain
+        // public decryption
+        const fhevmEnvironment: HardhatFhevmRuntimeEnvironment = hre.fhevm;
+
+        // Use the built-in `awaitDecryptionOracle` helper to wait for the FHEVM public decryption oracle
+        // to complete all pending Solidity public decryption requests.
+        await fhevmEnvironment.awaitDecryptionOracle();
+
+        const clearCount = await fheVoterContract.connect(signers.deployer).getDecryptedCount();
+
         expect(clearCount).to.eq(1);
+
     });
+
 });
